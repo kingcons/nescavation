ddb650d736e59cb678a44a8fb0b986b52f02ad49
Brit Butler <redline6561@gmail.com>
2016-09-19 17:42:01 -0400

    Whitespace fix.

--- src/js/cpu/init.js
+++ src/js/cpu/init.js
@@ -248,7 +248,7 @@ function initAccessor (addrMode, raw) {
   // KLUDGE: The only raw setter calls are in accumulator mode.
   // Specifically in asl, lsr, rol, and ror.
   if (raw) {
-   accessor = {
+    accessor = {
       get: (cpu) => addrMode.call(cpu.memory, cpu),
       set: (cpu, value) => { cpu.acc = value; }
     };
